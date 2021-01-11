3244eb1cf720d33fba8a28d678ca39beebd275f9
Hiroyuki Chihara <77265300+hiroyuki-chihara@users.noreply.github.com>
2021-01-11 21:04:17 +0900

    Add files via upload
    These programs are concerned with image processing of RGB image. They split a sample rgb.jpg into grayscale images of R, G and B, process them and combine them. rgb_haar.m gives multiresolution approximation by using the Haar wavelets, and rgb_svd.m computes the low rank approximation based on the singular value decomposition.

---
+++ rgb_haar.m
@@ -0,0 +1,85 @@
+% load image file
+I=imread('shaved_ice.jpg');
+RGB=imresize(I, 1/5);
+[R,G,B] = imsplit(RGB);
+% imshow(RGB)
+% whos R, whos G, whos B
+
+% resolution of R
+[RC,RS]=wavedec2(R,5,'haar');
+MRL1=appcoef2(RC,RS,'haar',1);
+MRL2=appcoef2(RC,RS,'haar',2);
+MRL3=appcoef2(RC,RS,'haar',3);
+MRL4=appcoef2(RC,RS,'haar',4);
+MRL5=appcoef2(RC,RS,'haar',5);
+RL1=mat2gray(MRL1);
+RL2=mat2gray(MRL2);
+RL3=mat2gray(MRL3);
+RL4=mat2gray(MRL4);
+RL5=mat2gray(MRL5);
+% resolution of G
+[GC,GS]=wavedec2(G,5,'haar');
+MGL1=appcoef2(GC,GS,'haar',1);
+MGL2=appcoef2(GC,GS,'haar',2);
+MGL3=appcoef2(GC,GS,'haar',3);
+MGL4=appcoef2(GC,GS,'haar',4);
+MGL5=appcoef2(GC,GS,'haar',5);
+GL1=mat2gray(MGL1);
+GL2=mat2gray(MGL2);
+GL3=mat2gray(MGL3);
+GL4=mat2gray(MGL4);
+GL5=mat2gray(MGL5);
+% resolution of B
+[BC,BS]=wavedec2(B,5,'haar');
+MBL1=appcoef2(BC,BS,'haar',1);
+MBL2=appcoef2(BC,BS,'haar',2);
+MBL3=appcoef2(BC,BS,'haar',3);
+MBL4=appcoef2(BC,BS,'haar',4);
+MBL5=appcoef2(BC,BS,'haar',5);
+BL1=mat2gray(MBL1);
+BL2=mat2gray(MBL2);
+BL3=mat2gray(MBL3);
+BL4=mat2gray(MBL4);
+BL5=mat2gray(MBL5);
+
+% R+G+B
+% RGBL1
+RGBL1(:,:,1)=(RL1);
+RGBL1(:,:,2)=(GL1);
+RGBL1(:,:,3)=(BL1);
+% RGBL2
+RGBL2(:,:,1)=(RL2);
+RGBL2(:,:,2)=(GL2);
+RGBL2(:,:,3)=(BL2);
+% RGBL3
+RGBL3(:,:,1)=(RL3);
+RGBL3(:,:,2)=(GL3);
+RGBL3(:,:,3)=(BL3);
+% RGBL4
+RGBL4(:,:,1)=(RL4);
+RGBL4(:,:,2)=(GL4);
+RGBL4(:,:,3)=(BL4);
+% RGBL5
+RGBL5(:,:,1)=(RL5);
+RGBL5(:,:,2)=(GL5);
+RGBL5(:,:,3)=(BL5);
+
+figure;
+subplot(2,3,1);
+imshow(RGB);
+title('Original RGB Image');
+subplot(2,3,2);
+imshow(RGBL1);
+title('Level 1 Approximation');
+subplot(2,3,3);
+imshow(RGBL2);
+title('Level 2 Approximation');
+subplot(2,3,4);
+imshow(RGBL3);
+title('Level 3 Approximation');
+subplot(2,3,5);
+imshow(RGBL4);
+title('Level 4 Approximation');
+subplot(2,3,6);
+imshow(RGBL5);
+title('Level 5 Approximation');
---
+++ rgb_svd.m
@@ -0,0 +1,117 @@
+% load image file
+I=imread('shaved_ice.jpg');
+RGB=imresize(I, 1/5);
+[R,G,B] = imsplit(RGB);
+% imshow(RGB)
+% whos R, whos G, whos B
+
+% uint8 -> float64
+DR=double(R);
+% R010
+[UR010,SR010,VR010]=svds(DR,010);
+DR010=UR010*SR010*VR010';
+R010=uint8(DR010);
+% R030
+[UR030,SR030,VR030]=svds(DR,30);
+DR030=UR030*SR030*VR030';
+R030=uint8(DR030);
+% R050
+[UR050,SR050,VR050]=svds(DR,50);
+DR050=UR050*SR050*VR050';
+R050=uint8(DR050);
+% R070
+[UR070,SR070,VR070]=svds(DR,070);
+DR070=UR070*SR070*VR070';
+R070=uint8(DR070);
+% R090
+[UR090,SR090,VR090]=svds(DR,090);
+DR090=UR090*SR090*VR090';
+R090=uint8(DR090);
+
+% uint8 -> float64
+DG=double(G);
+% G010
+[UG010,SG010,VG010]=svds(DG,10);
+DG010=UG010*SG010*VG010';
+G010=uint8(DG010);
+% G030
+[UG030,SG030,VG030]=svds(DG,30);
+DG030=UG030*SG030*VG030';
+G030=uint8(DG030);
+% G050
+[UG050,SG050,VG050]=svds(DG,50);
+DG050=UG050*SG050*VG050';
+G050=uint8(DG050);
+% G070
+[UG070,SG070,VG070]=svds(DG,070);
+DG070=UG070*SG070*VG070';
+G070=uint8(DG070);
+% G090
+[UG090,SG090,VG090]=svds(DG,090);
+DG090=UG090*SG090*VG090';
+G090=uint8(DG090);
+
+% uint8 -> float64
+DB=double(B);
+% B010
+[UB010,SB010,VB010]=svds(DB,10);
+DB010=UB010*SB010*VB010';
+B010=uint8(DB010);
+% B030
+[UB030,SB030,VB030]=svds(DB,30);
+DB030=UB030*SB030*VB030';
+B030=uint8(DB030);
+% B050
+[UB050,SB050,VB050]=svds(DB,50);
+DB050=UB050*SB050*VB050';
+B050=uint8(DB050);
+% B070
+[UB070,SB070,VB070]=svds(DB,070);
+DB070=UB070*SB070*VB070';
+B070=uint8(DB070);
+% B090
+[UB090,SB090,VB090]=svds(DB,090);
+DB090=UB090*SB090*VB090';
+B090=uint8(DB090);
+
+% R+G+B
+% RGB010
+RGB010(:,:,1)=(R010);
+RGB010(:,:,2)=(G010);
+RGB010(:,:,3)=(B010);
+% RGB030
+RGB030(:,:,1)=(R030);
+RGB030(:,:,2)=(G030);
+RGB030(:,:,3)=(B030);
+% RGB050
+RGB050(:,:,1)=(R050);
+RGB050(:,:,2)=(G050);
+RGB050(:,:,3)=(B050);
+% RGB070
+RGB070(:,:,1)=(R070);
+RGB070(:,:,2)=(G070);
+RGB070(:,:,3)=(B070);
+% RGB090
+RGB090(:,:,1)=(R090);
+RGB090(:,:,2)=(G090);
+RGB090(:,:,3)=(B090);
+
+figure;
+subplot(2,3,1);
+imshow(RGB);
+title('Original R, G and B are full rank 448 by 794 matrices respectively.');
+subplot(2,3,2);
+imshow(RGB010);
+title('Rank 10 Approximation');
+subplot(2,3,3);
+imshow(RGB030);
+title('Rank 30 Approximation');
+subplot(2,3,4);
+imshow(RGB050);
+title('Rank 50 Approximation');
+subplot(2,3,5);
+imshow(RGB070);
+title('Rank 70 Approximation');
+subplot(2,3,6);
+imshow(RGB090);
+title('Rank 90 Approximation');
